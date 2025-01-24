73dd2d0ade38124b07e8b870ad7451a77c5a0108
otipz19 <tasitwork@gmail.com>
2025-01-24 13:26:32 +0200

    Configured Flyway

--- product_order_app/src/main/java/app/contextlisteners/FlywayMigrationsApplier.java
+++ product_order_app/src/main/java/app/contextlisteners/FlywayMigrationsApplier.java
@@ -2,15 +2,13 @@ package app.contextlisteners;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
 import org.flywaydb.core.Flyway;
 
-@WebListener
 public class FlywayMigrationsApplier implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         var flyway = Flyway.configure()
-                .dataSource("jdbc:postgresql://localhost:5432/product_order_db", "product_order_admin", "admin")
                 .schemas("public")
+                .dataSource("jdbc:postgresql://localhost/product_order_db", "product_order_admin", "admin")
                 .locations("classpath:db/migration")
                 .load();
         flyway.migrate();
--- product_order_app/src/main/java/app/servlets/HelloWorldServlet.java
+++ product_order_app/src/main/java/app/servlets/HelloWorldServlet.java
@@ -7,14 +7,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @WebServlet("/")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var writer = resp.getWriter();
-        writer.write("Hello there!");
-        resp.setContentType("text/plain");
-        resp.setStatus(200);
+        String url = "jdbc:postgresql://localhost/product_order_db";
+        Properties props = new Properties();
+        props.setProperty("user", "product_order_admin");
+        props.setProperty("password", "admin");
+        try (Connection conn = DriverManager.getConnection(url, props)) {
+            var statement = conn.createStatement();
+            String sql = """
+                    SELECT * FROM vendor;
+                    """;
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                var writer = resp.getWriter();
+                writer.println(resultSet.getString("name"));
+            }
+            resp.setContentType("text/plain");
+            resp.setStatus(200);
+        } catch (SQLException e) {
+            System.out.println("SQL has failed!");
+            resp.setStatus(500);
+        }
     }
 }
